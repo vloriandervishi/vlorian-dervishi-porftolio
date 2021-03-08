6cd4f90c3398efb951974e9aafcaf7be85f84bf0
Valdo <vlorian22@GMAIL.COM>
2021-03-07 21:12:56 -0600

    Added about me section

---
+++ src/components/About/About-Me-index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About(){
+    return (
+        <section>
+            <h2 className="page-section"id="about" >About Me</h2>
+            <div> <img className="img" src="../../assets/images/IMG_0006.jpg"></img>
+                <p>Once a prosperouse life, I had in Kosovo now destroyed due to the war. Never thinking that I would be lucky enough to live in the United States;with a dream to become a software engineer.To pursue that dream, I graduated from Middle Tennessee State University with a bachelors degree information technology. Also
+                obtained associate of science degree from nashville state community college with the concentration in computer science and Full-Stack-Developer certification from vanderbilt university boot camp.</p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
