d2c31bf76357a55ba05d7fc994e9f413d494a689
Valdo <vlorian22@GMAIL.COM>
2021-03-07 21:14:53 -0600

    created and preloaded the code for portfolio section

---
+++ src/components/portfolio/portfolio-index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Portfolio(){
+ return (
+    <section className="page-section" id="work">
+    <h2>Front-End-Project</h2>
+    <div className="grid-container">
+
+      <a href="https://vloriandervishi.github.io/Front-End-Project-Off-The-Beaten-Path/" class="grid-item project-1">
+        <div>
+          <h3>Off the beaten track</h3>
+          <span>Front-End-Project</span>
+        </div>
+      </a>
+    </div>
+   
+  </section>
+
+ );
+}
+
+export default Portfolio;
