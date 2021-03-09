34b509e9a22fb34db953127cc2ced705f9c075d8
Valdo <vlorian22@GMAIL.COM>
2021-03-08 21:17:19 -0600

    added component ot index.js <footer>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Footer from './components/footer/footer';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />,
+    <Footer></Footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
