7e97211d6ddc7df4d45494defcaec032428fecfd
Valdo <vlorian22@GMAIL.COM>
2021-03-09 01:34:15 -0600

    added download link for resume

--- src/components/Resume/Resume-index.js
+++ src/components/Resume/Resume-index.js
@@ -1,31 +1,42 @@
-import React from 'react';
-
-function Resume(props){
-return (
-    <ul> 
-        <li className="nav"><h2>Front-End</h2></li>
-        <li></li>
-        <li>CSS</li>
-         <li>Javascript</li>
-         <li>ReactJS</li>
-         <li>Boostrap</li>
-         <li>JQUERY</li>
-         <br></br>
-         <li className="nav"><h2>Back-End</h2></li>
-         <li>WEB API's</li>
-         <li>MongoDB</li>
-         <li>Sequelize</li>
-         <li>MVC</li>
-         <li>CRM</li>
-         <li>MySQL</li>
-         <li>NoSQL</li>
-         <br></br>
-         <li className="nav"><h2>Server-Side</h2></li>
-         <li>ExpressJS</li>
-         <li>REST API</li>
-
-    </ul>
+import React from "react";
+import Download from '@axetroy/react-download';
+import {link} from 'react-router-dom';
+// import download from '../../assets/images/downloadbutton.jfif';
+// import resume from '../../assets/images/Vlorian Dervishi CV (2).pdf';
+function Resume(props) {
     
-)
+  return (
+       
+   
+    <div style={{ textAlign: "center" }}>
+        <a href="./Resume.pdf" download>Resume</a>
+      <ul style={{ listStyleType: "none" }}>
+        
+        <li style={{ textAlign: "center" }}>
+          <h2>Front-End</h2>
+        </li>
+
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>ReactJS</li>
+        <li>Boostrap</li>
+        <li>JQUERY</li>
+        <br></br>
+        <li style={{ textAlign: "center" }}>
+          <h2>Back-End</h2>
+        </li>
+        <li>WEB API's</li>
+        <li>MongoDB</li>
+        <li>Sequelize</li>
+        <li>MVC</li>
+        <li>CRM</li>
+        <li>MySQL</li>
+        <li>NoSQL</li>
+        <li>ExpressJS</li>
+        <li>REST API</li>
+      </ul>
+    </div>
+  );
+  
 }
 export default Resume;
