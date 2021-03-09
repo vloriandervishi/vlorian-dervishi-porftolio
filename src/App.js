50610f0048d857e650a9a7c49029c54b70ce119d
Valdo <vlorian22@GMAIL.COM>
2021-03-08 23:33:34 -0600

    formatted pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PortfolioContainer from './components/PortfolioContainer';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/footer/footer.js";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-const App=()=> <PortfolioContainer></PortfolioContainer>;
+const App = () => <PortfolioContainer></PortfolioContainer>;
 
 export default App;
