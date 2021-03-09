6ec8867ddfa681b859c8510c8bb691e3e84e3db6
Valdo <vlorian22@GMAIL.COM>
2021-03-08 21:45:16 -0600

    fixed grid item and background images

--- src/components/portfolio/portfolio-index.js
+++ src/components/portfolio/portfolio-index.js
@@ -1,24 +1,27 @@
 import React from "react";
 import Victor from "../../assets/images/VictorsDealerShipScreenShot.jpg";
 import Beaten from "../../assets/images/screenshot-1.jpg";
+import Icon from "@iconify/react";
 function Portfolio() {
   return (
-    <section className="page-section">
-      <h2>Front-End-Project</h2>
+    
       <div className="grid-container">
         <a
           href="https://vloriandervishi.github.io/Front-End-Project-Off-The-Beaten-Path/"
-          className="grid-item"
+          className="grid-item" style={{backgroundImage:`url(${Beaten})`}}
         >
           <div>
             <h3>Off the beaten track</h3>
-            <img src={Beaten}></img>
+            
+          </div>
+        </a>
+        <a href="https://victorsdealership.herokuapp.com/" className="grid-item" style={{backgroundImage:`url(${Victor})`}}>
+          <div>
+            <h3>Victors DealerShip</h3>
           </div>
         </a>
       </div>
-      
-      
-    </section>
+    
   );
 }
 
