84dd7b72a951756efd78557f484bd6f025db4b55
Valdo <vlorian22@GMAIL.COM>
2021-08-01 15:20:39 -0400

    Changed About me

--- src/components/About/About-Me-index.js
+++ src/components/About/About-Me-index.js
@@ -6,7 +6,7 @@ function About(){
             <h2 className="page-section"id="about" >About Me</h2>
             <div> <img src={picture}></img>
                 <p>Once a prosperouse life, I had in Kosovo now destroyed due to the war. Never thinking that I would be lucky enough to live in the United States;with a dream to become a software engineer.To pursue that dream, I graduated from Middle Tennessee State University with a bachelors degree information technology. Also
-                obtained associate of science degree from nashville state community college with the concentration in computer science and Full-Stack-Developer certification from vanderbilt university boot camp.</p>
+                obtained associate of science degree from nashville state community college with the concentration in computer science and Full-Stack-Developer certification from vanderbilt university boot camp. A Web Developer with background in trades-craft and customer service.  </p>
             </div>
         </section>
     );
--- src/components/Contact/contact-index.js
+++ src/components/Contact/contact-index.js
@@ -1,46 +1,48 @@
-import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
+import React, { useState } from 'react';
+
+import { validateEmail } from '../../utils/helpers';
+
 function Contact() {
   const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
+    name: '',
+    email: '',
+    message: '',
   });
 
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
   const { name, email, message } = formState;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!errorMessage) {
-      console.log("Submit Form", formState);
+      console.log('Submit Form', formState);
     }
   };
 
   const handleChange = (e) => {
-    if (e.target.name === "email") {
+    if (e.target.name === 'email') {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
-        setErrorMessage("Your email is invalid.");
+        setErrorMessage('Your email is invalid.');
       } else {
-        setErrorMessage("");
+        setErrorMessage('');
       }
     } else {
       if (!e.target.value.length) {
         setErrorMessage(`${e.target.name} is required.`);
       } else {
-        setErrorMessage("");
+        setErrorMessage('');
       }
     }
     if (!errorMessage) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log("Handle Form", formState);
+      console.log('Handle Form', formState);
     }
   };
+
   return (
     <section>
-      <h1 className="Centername">Contact me</h1>
-      <form className="contact-form" onSubmit={handleSubmit}>
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
@@ -62,7 +64,6 @@ function Contact() {
         <div>
           <label htmlFor="message">Message:</label>
           <textarea
-            className="textarea"
             name="message"
             rows="5"
             defaultValue={message}
@@ -79,4 +80,5 @@ function Contact() {
     </section>
   );
 }
+
 export default Contact;
